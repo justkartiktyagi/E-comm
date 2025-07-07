0da3d6c3c45ae733439332df2a1ec0ea7c5d62c6
BxFT <BxFT@bft.com>
2025-07-07 07:49:14 +0530

    First COmmit

--- src/Pages/components/Navbar.jsx
+++ src/Pages/components/Navbar.jsx
@@ -1,27 +1,49 @@
 import React from "react";
-import imagege_1 from "../../../src/Images/instagram.png"
-import imagege_2 from "../../../src/Images/gmail.png";
-import imagege_3 from "../../../src/Images/facebook.png";
-import imagege_4 from "../../../src/Images/telephone.png";
+import '/src/index.css'
 
 function Navbar() {
+
+
     return (
         <>
             <div className="w-100% h-10 top-0 relative bg-black   ">
                 <div className="text-left">
-                    <p className="text-white pl-30 pt-2   ">Don't miss our holiday offer - up to 50% OFF!</p>
+                    <p className="text-white pl-25 pt-2  font-serif  ">Don't miss our holiday offer - up to 50% OFF!</p>
                 </div>
 
+                <img className="absolute right-[180px] top-1/2 -translate-y-1/2 w-5 h-5"
+                     src="../../../public/facebook.png" alt=""/>
+                <img className="absolute right-[280px] top-1/2 -translate-y-1/2 w-5 h-5"
+                     src="../../../public/telephone.png" alt=""/>
+                <img className="absolute right-[130px] top-1/2 -translate-y-1/2 w-5 h-5"
+                     src="../../../public/instagram.png" alt=""/>
+                <img className="absolute right-[230px] top-1/2 -translate-y-1/2 w-5 h-5" src="../../../public/gmail.png"
+                     alt=""/>
 
-                <img className="absolute right-[50px] top-1/2 -translate-y-1/2 w-5 h-5" src={imagege_4} alt=""/>
-                <img className="absolute right-[100px] top-1/2 -translate-y-1/2 w-5 h-5" src={imagege_2} alt=""/>
-                <img className="absolute right-[150px] top-1/2 -translate-y-1/2 w-5 h-5" src={imagege_3} alt=""/>
-                <img className="absolute right-[200px] top-1/2 -translate-y-1/2 w-5 h-5" src={imagege_1} alt=""/>
 
             </div>
-            <div className="w-100% h-30 bg-white">
+            <div className="w-full h-20 top-0  bg-white ">
+                <div className="container mx-auto pl-40 pr-20 flex items-center justify-between h-full">
+
+                    <p className="text-3xl font-roboto font-bold">ShopHop</p>
+                    <div className=" flex text-center">
+                        <ul className="flex font-bold  font-roboto space-x-15 ">
+                            <li>Home</li>
+                            <li>About Us</li>
+                            <li>Products</li>
+                            <li>Contact Us</li>
+                            <li>News</li>
+                            <li>Services</li>
+                        </ul>
+                    </div>
+                    <div className="space-x-4">
+                        <button>Search</button>
+                        <button>Bag</button>
+                    </div>
+                </div>
 
             </div>
+
 
         </>
     )
--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,9 +1,16 @@
 import Navbar from "./components/Navbar.jsx";
 
-function Home(){
+
+function Home() {
     return (
         <>
-           <Navbar/>
+            <Navbar/>
+            <div className="items-center justify-center text-center grid grid-cols-2 pt-25">
+                <div>
+                    <img src="../../public/download-removebg-preview.png" alt="image"/>
+                </div>
+                <div>02</div>
+            </div>
         </>
     )
 }
