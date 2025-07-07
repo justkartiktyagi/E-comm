eb97b6923b15b6fa29ec96f65197af3eba5a1390
BxFT <BxFT@bft.com>
2025-07-07 22:49:13 +0530

    First COmmit

--- src/Pages/components/Navbar.jsx
+++ src/Pages/components/Navbar.jsx
@@ -6,39 +6,61 @@ function Navbar() {
 
     return (
         <>
-            <div className="w-100% h-10 top-0 relative bg-black   ">
+            <div className="w-100% h-10 top-[-10] relative bg-black   ">
                 <div className="text-left">
-                    <p className="text-white pl-25 pt-2  font-serif  ">Don't miss our holiday offer - up to 50% OFF!</p>
+                    <p className="text-white pl-20 pt-2  font-ubuntu  ">Don't miss our holiday offer - up to 50%
+                        OFF!</p>
                 </div>
 
-                <img className="absolute right-[180px] top-1/2 -translate-y-1/2 w-5 h-5"
+                <img className="absolute right-[138px] top-1/2 -translate-y-1/2 w-5 h-5"
                      src="../../../public/facebook.png" alt=""/>
-                <img className="absolute right-[280px] top-1/2 -translate-y-1/2 w-5 h-5"
+                <img className="absolute right-[238px] top-1/2 -translate-y-1/2 w-5 h-5"
                      src="../../../public/telephone.png" alt=""/>
-                <img className="absolute right-[130px] top-1/2 -translate-y-1/2 w-5 h-5"
+                <img className="absolute right-[88px] top-1/2 -translate-y-1/2 w-5 h-5"
                      src="../../../public/instagram.png" alt=""/>
-                <img className="absolute right-[230px] top-1/2 -translate-y-1/2 w-5 h-5" src="../../../public/gmail.png"
+                <img className="absolute right-[188px] top-1/2 -translate-y-1/2 w-5 h-5" src="../../../public/gmail.png"
                      alt=""/>
 
 
             </div>
             <div className="w-full h-20 top-0  bg-white ">
-                <div className="container mx-auto pl-40 pr-20 flex items-center justify-between h-full">
+                <div className="container mx-auto pl-20 pr-20 flex items-center justify-between h-full">
 
-                    <p className="text-3xl font-roboto font-bold">ShopHop</p>
+                    <a href="#"><p className="text-3xl font-ubuntu font-medium">DripCode</p></a>
                     <div className=" flex text-center">
-                        <ul className="flex font-bold  font-roboto space-x-15 ">
-                            <li>Home</li>
-                            <li>About Us</li>
-                            <li>Products</li>
-                            <li>Contact Us</li>
-                            <li>News</li>
-                            <li>Services</li>
+                        <ul className="flex font-medium  font-ubuntu space-x-15 ">
+                            <a href="#">
+                                <li>Home</li>
+                            </a>
+                            <a href="#">
+                                <li>About Us</li>
+                            </a>
+                            <a href="#">
+                                <li>Products</li>
+                            </a>
+                            <a href="#">
+                                <li>Contact Us</li>
+                            </a>
+                            <a href="#">
+                                <li>News</li>
+                            </a>
+                            <a href="#">
+                                <li>Services</li>
+                            </a>
                         </ul>
                     </div>
                     <div className="space-x-7">
-                        <button>Search</button>
-                        <button>Bag</button>
+                        <button>
+                            <a href="#"><img className="w-7 h-7" src="../../../public/search.png" alt=""/></a>
+                        </button>
+                        <button>
+                            <a href="#"> <img className="w-7 h-7" src="../../../public/people.png"
+                                              alt=""/></a>
+                        </button>
+                        <button>
+                            <a href="#"> <img className=" w-7 h-7" src="../../../public/shopping-bag.png"
+                                              alt=""/></a>
+                        </button>
                     </div>
                 </div>
 
--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,11 +1,23 @@
 import Navbar from "./components/Navbar.jsx";
 
-
 function Home() {
     return (
         <>
             <Navbar/>
-            
+            <div>
+                <div className="relative w-full container mx-auto">
+                    <img className="h-200  w-full"
+                         src="../../public/Black Red Minimalist Fashion Product Introduction Landscape Banner.png"
+                         alt="Banner Image"/>
+
+                    <button
+                        className=" absolute bottom-32 left-32 font-medium font-ubuntu text-xl bg-red-600  text-black py-5 px-7">
+                        Explore Now
+                    </button>
+
+                </div>
+
+            </div>
         </>
     )
 }
