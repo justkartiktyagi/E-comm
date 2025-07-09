e249adbb281d85a8fc57261692ba82f0803affa3
BxFT <BxFT@bft.com>
2025-07-09 08:03:55 +0530

    First COmmit

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,6 +1,35 @@
 import Navbar from "./components/Navbar.jsx";
 
 function Home() {
+
+    const images = [
+
+        {
+            id: 1,
+            src: "../../../public/credit-card.png",
+            caption1: "Secure Payment",
+            caption2: "100% secure payment"
+        },
+        {
+            id: 2,
+            src: "../../public/delivery-status.png",
+            caption1: "30 Days Return",
+            caption2: "If Goods have Problems"
+        },
+        {
+            id: 3,
+            src: "../../public/support.png",
+            caption1: "24/7 Support",
+            caption2: "Dedicated Support",
+        },
+        {
+            id: 4,
+            src: "../../public/free-delivery.png",
+            caption1: "Free Delivery",
+            caption2: "For all order Over 80$",
+        }
+    ];
+
     return (
         <>
             <Navbar/>
@@ -15,6 +44,19 @@ function Home() {
                         Explore Now
                     </button>
 
+                </div>
+                <div className="bg-white">
+                    <div className="h-50 w-full grid grid-cols-4 items-center container mx-auto  text-center">
+                        {images.map((image) => (
+                            <div key={image.id} className="text-center">
+                                <div className="justify-center flex">
+                                    <img className="h-15 w-15" src={image.src} alt={image.caption1}></img>
+                                </div>
+                                <p className="text-2xl mt-5 font-bold">{image.caption1}</p>
+                                <p className="text-m ">{image.caption2}</p>
+                            </div>
+                        ))}
+                    </div>
                 </div>
 
             </div>
