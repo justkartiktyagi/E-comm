d2fe13ec62b74416efcf17291716dd62685075e3
Opulent <maniidon7890@gmail.com>
2025-07-21 08:21:57 +0530

    product_list

--- src/Pages/product_list.jsx
+++ src/Pages/product_list.jsx
@@ -5,8 +5,19 @@ function ProductList(){
 
     return(
         <div>
+            <div className="flex items-center justify-center h-50 w-full">
+                <p className="text-3xl text-white font-ubuntu ">This is Product page</p>
+            </div>
+            <div className="bg-white ">
+            <div className="flex px-20 py-4 container mx-auto">
+                <aside className="h-200 w-110 bg-yellow-300">
 
-            <p className="text-2xl text-center mt-50 text-white">This is Product page</p>
+                </aside>
+                <main className="h-200 w-200 ml-10 bg-red-600">
+
+                </main>
+            </div>
+            </div>
         </div>
     )
 }
